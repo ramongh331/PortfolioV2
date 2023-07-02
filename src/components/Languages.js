9907030fb3b1e15e8ca30a8ece3c84027d9a7429
Ramon Hernandez <ramongh331@gmail.com>
2023-07-02 18:00:35 -0400

    added the custom images into the cards with the hover effects

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 // import nextBtn from "../public/images/next.png"
 // import backBtn from "../public/images/back.png"
+import HTML from "../public/images/languageImages/HTML.jpg"
+import JS from "../public/images/languageImages/JavaScript.jpg"
+import CSS from "../public/images/languageImages/CSS.jpg"
+import Sass from "../public/images/languageImages/Sass.jpg"
 
 export default function LanguagesRow() {
 
@@ -31,12 +35,11 @@ export default function LanguagesRow() {
     <section className="">
       <h3 className="text-2xl text-white my-5">Languages</h3>
       <section className="w-full relative">
-        <div className="w-fit flex duration-300 ease-in-out">
-          <div className="bg-green-600 p-4 w-[19rem] h-[11rem] rounded-xl box-border mr-7 overflow-hidden">HTML</div>
-          <div className="bg-green-600 p-4 w-[19rem] h-[11rem] rounded-xl box-border mr-7 overflow-hidden">JavaScript</div>
-          <div className="bg-green-600 p-4 w-[19rem] h-[11rem] rounded-xl box-border mr-7 overflow-hidden">EJS</div>
-          <div className="bg-green-600 p-4 w-[19rem] h-[11rem] rounded-xl box-border mr-7 overflow-hidden">CSS</div>
-          <div className="bg-green-600 p-4 w-[19rem] h-[11rem] rounded-xl box-border mr-7 overflow-hidden">Sass</div>
+        <div className="w-fit h-52 flex items-center duration-300 ease-in-out">
+          <div className="bg-green-600 w-[19rem] h-[11rem] rounded-xl box-content mr-7 overflow-hidden duration-200 ease-in-out hover:border-4 hover:scale-110"><img className="" src={HTML} alt=""/></div>
+          <div className="bg-green-600 w-[19rem] h-[11rem] rounded-xl box-content mr-7 overflow-hidden duration-200 ease-in-out hover:border-4 hover:scale-110"><img className="" src={JS} alt=""/></div>
+          <div className="bg-green-600 w-[19rem] h-[11rem] rounded-xl box-content mr-7 overflow-hidden duration-200 ease-in-out hover:border-4 hover:scale-110"><img className="" src={CSS} alt=""/></div>
+          <div className="bg-green-600 w-[19rem] h-[11rem] rounded-xl box-content mr-7 overflow-hidden duration-200 ease-in-out hover:border-4 hover:scale-110"><img className="" src={Sass} alt=""/></div>
         </div>
       </section>
     </section>
