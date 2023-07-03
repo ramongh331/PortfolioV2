56db397015df12c171bd8bcda8fab1e49ae4dd8f
Ramon Hernandez <ramongh331@gmail.com>
2023-07-03 18:51:12 -0400

    added links to the header and made it fixed

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
 import DesignRow from "../components/DesignTools";
 import FrameworksRow from "../components/Frameworks";
 import LanguagesRow from "../components/Languages";
@@ -24,20 +25,24 @@ export default function Technology() {
   return (
     <>
       <main className="bg-gray-800 w-full h-full relative">
-        <header className="bg-gradient-to-b from-gray-800 w-full h-28 flex justify-between z-10 absolute">
-          <h1 className="font-bold text-2xl ml-10 text-[#1be783] mt-6">
-            Skills+
-          </h1>
-          <section className="w-[35rem] flex justify-between text-xl font-semibold text-white mt-6">
-            <h2>Home</h2>
-            <h2>About</h2>
-            <h2>Projects</h2>
-            <h2>Contact</h2>
+        <header className="bg-gradient-to-b from-gray-800 w-full h-20 flex justify-between z-10 fixed">
+          <Link to="/technology">
+            <h1 className="font-bold text-2xl ml-10 text-[#1be783] mt-6">
+              Skills+
+            </h1>
+          </Link>
+          <section className="w-[35rem] flex justify-between text-xl font-semibold text-white mt-6 mx-5">
+            <Link to="/"><h2>Home</h2></Link>
+            <Link to="/about"><h2>About</h2></Link>
+            <Link to="/projects"><h2>Projects</h2></Link>
+            <Link to="/contact"><h2>Contact</h2></Link>
           </section>
           <section className="mr-10 mt-6">
-            <div className="w-10 h-10 rounded-3xl text-md flex justify-center items-center font-semibold border-white border-2 bg-blue-300">
-              RH
-            </div>
+            <Link to="/about">
+              <div className="w-10 h-10 rounded-3xl text-md flex justify-center items-center font-semibold border-white border-2 bg-blue-300">
+                RH
+              </div>
+            </Link>
           </section>
         </header>
         <section className="w-full h-[45rem] overflow-hidden relative">
