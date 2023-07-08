155d99dbf39a5173137b227cd62776f0200c03ee
Ramon Hernandez <ramongh331@gmail.com>
2023-07-07 22:19:53 -0400

    added links to projects and contact and under construction message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -35,6 +35,7 @@ export default function Home() {
   return (
     <>
       <main className="bg-yellow-1000 w-full h-screen flex flex-col justify-evenly items-center font-display font-bold">
+        <p className="text-5xl bg-white">Under Construction, but you can see what is done so far.</p>
         <button onClick={handleClick}>English/Español</button>
         <h1
           className="text-[220px] w-8/12 text-center ease-in-out duration-300"
@@ -46,8 +47,8 @@ export default function Home() {
         <section className="flex justify-evenly w-full text-5xl mb-20">
           <Link to="/about">{about ? <h2>Sobre Mi</h2> : <h2>About</h2>}</Link>
           <Link to="/technology">{tech ? <h2>Tecnología</h2> : <h2>Technology</h2>}</Link>
-          {projects ? <h2>Proyectos</h2> : <h2>Projects</h2>}
-          {contact ? <h2>Contacto</h2> : <h2>Contact</h2>}
+          <Link to="/projects">{projects ? <h2>Proyectos</h2> : <h2>Projects</h2>}</Link>
+          <Link to="/contact">{contact ? <h2>Contacto</h2> : <h2>Contact</h2>}</Link>
         </section>
       </main>
     </>
