372d84028c9e209595b7825c5ce4d1c08c686deb
Ramon Hernandez <ramongh331@gmail.com>
2023-07-07 22:18:28 -0400

    added under construction message and email and linkedin

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,11 @@
 export default function Contact(){
     return <>
-    <main>
-        <h1>Contact</h1>
+    <main className="flex flex-col items-center gap-10">
+        <h2>Contact</h2>
+        <p className="text-5xl">I haven't even started this page yet.
+        </p>
+        <p className="text-5xl">But if you like what you see so far...Email: <span className="font-bold">ramongh331@gmail.com</span></p>
+        <p className="text-5xl">Linkedin: https://www.linkedin.com/in/ramon-hernandez-web/</p>
     </main>
     </>
 }
