6535b764e1ca560ffcf9ab95ca1d42c861b1984c
Ramon Hernandez <ramongh331@gmail.com>
2023-07-16 21:26:57 -0400

    started contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,16 @@
-export default function Contact(){
-    return <>
-    <main className="flex flex-col items-center gap-10">
-        <h2>Contact</h2>
-        <p className="text-5xl">I haven't even started this page yet.
-        </p>
-        <p className="text-5xl">But if you like what you see so far...Email: <span className="font-bold">ramongh331@gmail.com</span></p>
-        <p className="text-5xl">Linkedin: https://www.linkedin.com/in/ramon-hernandez-web/</p>
-    </main>
+import { ContactUs } from "../components/Contact";
+import TeamRamon from "../public/images/ContactImage/TeamRamon.png";
+import HeroBG from "../public/images/ContactImage/background.jpeg";
+
+export default function Contact() {
+  return (
+    <>
+      <main className="w-full h-full">
+        <nav>Nav</nav>
+        <h2 className="bebas text-6xl text-center">Are you Team Ramón?</h2>
+        <img className="mx-auto w-[40%]" src={TeamRamon} alt="" />
+        <ContactUs />
+      </main>
     </>
+  );
 }
