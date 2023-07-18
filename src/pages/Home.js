f9100918fb3c8bae9e5948cb45cafa38a91c5f90
Ramon Hernandez <ramongh331@gmail.com>
2023-07-17 20:47:49 -0400

    Home page is responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -15,15 +15,20 @@ export default function Home() {
   return (
     <>
       <main className="bg-yellow-1000 w-full h-screen flex flex-col justify-evenly items-center font-display font-bold">
-        <p className="text-5xl bg-white">Under Construction, but you can see what is done so far.</p>
+        {/* <p className="text-3xl bg-white">Under Construction, but you can see what is done so far.</p> */}
         <h1
-          className="text-[220px] w-8/12 text-center ease-in-out duration-300"
+          className="max-sm:text-4xl sm:text-8xl 2xl:text-9xl text-[220px] 
+          max-sm:w-11/12 sm:w-11/12 lg:w-9/12 xl:w-8/12 2xl:w-7/12 
+          text-center
+          ease-in-out duration-300"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
           {content}
         </h1>
-        <section className="flex justify-evenly w-full text-5xl mb-20">
+        <section className="flex justify-evenly w-full 
+        max-sm:text-xs sm:text-3xl 2xl:text-6xl text-5xl 
+        sm:mb-0 mb-20">
           <Link to="/about"><h2>About</h2></Link>
           <Link to="/technology"><h2>Technology</h2></Link>
           <Link to="/projects"><h2>Projects</h2></Link>
