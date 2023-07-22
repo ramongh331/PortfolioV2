69ba13c3f68e8df0d754563b494b8f2ed6aaef80
Ramon Hernandez <ramongh331@gmail.com>
2023-07-22 15:09:58 -0400

    changed icons to white. Made contact page responsive. Added hamburger menu for max-small screens

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { ExternalLink } from "react-external-link";
+import GitHub from "../public/images/ContactImage/GitHub.png"
+import LinkedIn from "../public/images/ContactImage/LinkedIn.png"
 
 
 export const ContactUs = () => {
@@ -29,23 +31,39 @@ export const ContactUs = () => {
   };
 
   return (
-    <section className="w-full h-[28rem] flex flex-col bg-[#132932] text-white">
-      <h2 className="text-6xl my-5 ml-10 font-bold">Let's Connect</h2>
-      <section className="w-full h-full flex justify-around">
-        <section className="w-[25rem] flex flex-col gap-2">
-          <h3 className="w-full border-b-2 border-white text-3xl font-semibold">Hire Me</h3>
-          <p className="text-xl">Email: RamonGH331@gmail.com</p>
-          <p className="text-xl">Location: Connecticut | Willing to Relocate</p>
+    <section className="w-full 
+    max-sm:h-full sm:h-full xl:h-[28rem] 2xl:h-screen min-[1920px]:h-[28rem] 
+    flex flex-col bg-[#132932] text-white">
+      <h2 className="max-sm:text-3xl sm:text-5xl 2xl:text-6xl min-[1920px]:text-6xl 
+      my-5 
+      2xl:ml-10 
+      font-bold
+      text-center">Let's Connect</h2>
+      <section className="w-full h-full 
+      flex max-sm:flex-col sm:flex-col lg:flex-row
+      max-sm:gap-5 sm:gap-5
+      max-sm:items-center sm:items-center lg:items-start
+      justify-around">
+      {/* Hire Me Section */}
+        <section className="max-sm:w-[90%] sm:w-[25rem] lg:w-[15rem] xl:w-[20rem] min-[1920px]:w-[25rem] 
+        flex flex-col max-sm:gap-2 sm:gap-4">
+          <h3 className="w-full border-b-2 border-white 
+          max-sm:text-2xl sm:text-3xl
+          font-semibold">Hire Me</h3>
+          <p className="sm:text-xl lg:text-base xl:text-xl">Email: RamonGH331@gmail.com</p>
+          <p className="sm:text-xl lg:text-base xl:text-xl">Location: Connecticut | Willing to Relocate</p>
           <ExternalLink
             href="https://docs.google.com/document/d/1MJJ1Zp0emiOn1I9aolUsWSZ4OC0Z7B-AgSi0rxy2bb4/edit?usp=sharing"
             target="_blank"
             rel="noreferrer"
           >
-            <p className="text-xl">Resumè</p>
+            <p className="sm:text-xl lg:text-base xl:text-xl">Resumè</p>
           </ExternalLink>
         </section>
-        <section className="w-[25rem]">
-          <h3 className="w-full mb-2 border-b-2 border-white text-3xl font-semibold">Message Me</h3>
+
+        {/* Message Me Section */}
+        <section className="max-sm:w-[90%] sm:w-[25rem] lg:w-[20rem] min-[1920px]:w-[30rem]">
+          <h3 className="w-full mb-2 border-b-2 border-white max-sm:text-2xl sm:text-3xl font-semibold">Message Me</h3>
           <form className="flex flex-col gap-2" ref={form} onSubmit={sendEmail}>
             
               <input
@@ -72,22 +90,25 @@ export const ContactUs = () => {
             </section>
           </form>
         </section>
-        <section className="w-[25rem]">
-          <h3 className="w-full mb-2 border-b-2 border-white text-3xl font-semibold">Keep Connected</h3>
+
+        {/* Keep Connected Section */}
+        <section className="max-sm:w-[90%] sm:w-[25rem] lg:w-[15rem] xl:w-[20rem] min-[1920px]:w-[25rem]
+        mb-10">
+          <h3 className="w-full mb-2 border-b-2 border-white max-sm:text-2xl sm:text-3xl font-semibold">Keep Connected</h3>
           <section className="flex gap-5">
             <a
               href="https://www.linkedin.com/in/ramon-hernandez-web/"
               target="_blank"
               rel="noreferrer"
             >
-              <img className="w-10" src="https://i.imgur.com/9whU8k1.png" alt="LinkedIn Icon" />
+              <img className="w-10" src={LinkedIn} alt="LinkedIn Icon" />
             </a>
             <a
               href="https://github.com/ramongh331"
               target="_blank"
               rel="noreferrer"
             >
-              <img className="w-10" src="https://i.imgur.com/Ye3Jxte.png" alt="GitHub Icon" />
+              <img className="w-10" src={GitHub} alt="GitHub Icon" />
             </a>
           </section>
         </section>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { ContactUs } from "../components/Contact";
 import { Link } from "react-router-dom";
 import TeamRamon from "../public/images/ContactImage/TeamRamon.png";
 import { ExternalLink } from "react-external-link";
+import NavIcon from "../public/images/ProjectsImages/HamBurgMenu.png"
 
 export default function Contact() {
+  const [nav, setNav] = useState("-right-[100%]")
+  
+  const handleOpenNav = () => {
+    setNav("-right-2")
+  }
+  const handleCloseNav = () => {
+    setNav("-right-[100%]")
+  }
+  
   return (
     <>
-      <main className="w-full h-full bg-[#132932] text-white">
+      <main className="w-full h-full bg-[#132932] text-white relative overflow-hidden">
       <header className="border-b-2 border-[#58c7f9]
         w-full 
         max-sm:h-10 sm:h-14 2xl:h-20 
@@ -22,7 +33,7 @@ export default function Contact() {
               Contact
             </h1>
           </Link>
-          <section className="max-sm:w-[25rem] sm:w-[20rem] xl:w-[25rem] 2xl:w-[35rem]
+          <section className="max-sm:hidden sm:w-[20rem] xl:w-[25rem] 2xl:w-[35rem]
           flex 
           justify-between
           items-center 
@@ -41,7 +52,7 @@ export default function Contact() {
             target="_blank"
             rel="noreferrer"
           >
-              <div className="max-sm:w-7 sm:w-10 
+              <div className="max-sm:hidden sm:w-10 
               max-sm:h-7 sm:h-10 
               max-sm:text-xs sm:text-md
               rounded-3xl flex justify-center items-center font-semibold border-white border-2 bg-blue-300 text-black">
@@ -50,8 +61,29 @@ export default function Contact() {
             </ExternalLink>
           </section>
         </header>
-        <h2 className="bebas text-6xl text-center mt-10">Are you Team Ramón?</h2>
-        <img className="mx-auto w-[40%]" src={TeamRamon} alt="" />
+        {/* Max Small (Mobile) Nav */}
+  <div className={`sm:hidden absolute z-10 top-[.45rem] right-2 flex justify-center items-center duration-[1500ms] ease-in-out cursor-pointer`} onClick={handleOpenNav}><img className="h-5" src={NavIcon} alt="Navigation Menu Button"/></div>
+
+<div className={`lightBlueGlow w-[22rem] h-[102%] border-2 border-[#58c7f9] bg-[#132933] absolute duration-1000 ease-in-out z-10 p-5 flex flex-col gap-10 items-center ${nav} top-0`}>
+ <div className="text-white hover:text-black hover:bg-[#91dcff] font-bold text-xl text-center w-full cursor-pointer" onClick={handleCloseNav}>X</div>
+
+ <Link className="cursor-pointer w-[95%] hover:border-2 border-[#58c7f9]" to="/">
+   <h2 className="text-5xl text-white p-4">Home</h2>
+ </Link>
+ <Link className="cursor-pointer w-[95%] hover:border-2 border-[#58c7f9]" to="/about">
+   <h2 className="text-5xl text-white p-4">About</h2>
+ </Link>
+ <Link className="cursor-pointer w-[100%] hover:border-2 border-[#58c7f9]" to="/technology">
+   <h2 className="text-5xl text-white p-4">Technology</h2>
+ </Link>
+ <Link className="cursor-pointer w-[95%] hover:border-2 border-[#58c7f9]" to="/projects">
+   <h2 className="text-5xl text-white p-4">Projects</h2>
+ </Link>
+ 
+</div>
+        <h2 className="bebas max-sm:text-4xl sm:text-5xl md:text-6xl xl:text-7xl 2xl:text-8xl text-center 
+        max-sm:mt-5 sm:mt-5 2xl:mt-10 mb-5">Are you Team Ramón?</h2>
+        <img className="mx-auto max-sm:w-[90%] sm:w-[90%] lg:w-[60%]" src={TeamRamon} alt="" />
         <ContactUs />
       </main>
     </>
